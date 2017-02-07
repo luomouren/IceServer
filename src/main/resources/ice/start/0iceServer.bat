6e6b53328a4a1776bcbc95476a409c4d316d71b8
Amy <luomouren@foxmail.com>
2017-02-07 09:42:08 +0800

    提交修改启动bat命令

--- src/main/resources/ice/start/0iceServer.bat
+++
@@ -1,4 +0,0 @@
-::隐藏窗口运行bat文件
-echo CreateObject("WScript.Shell").Run "cmd /c G:\00workspace\00workspace\01JCXT\IceServer\src\main\resources\ice\start\1start.bat",0>$tmp.vbs
-cscript.exe /e:vbscript $tmp.vbs
-del $tmp.vbs
--- src/main/resources/ice/start/2start.bat
+++ src/main/resources/ice/start/2start.bat
@@ -1,6 +1,6 @@
-::2、部署分布式服务
+::2銆侀儴缃插垎甯冨紡鏈嶅姟
 G:
 cd G:\00workspace\00workspace\01JCXT\IceServer\src\main\resources\ice
 icegridadmin --Ice.Config=G:\00workspace\00workspace\01JCXT\IceServer\src\main\resources\ice\registry.cfg -e "application add  'G:\00workspace\00workspace\01JCXT\IceServer\src\main\resources\ice\grid.xml' "
-::2.1、重新部署分布式服务
+::2.1銆侀噸鏂伴儴缃插垎甯冨紡鏈嶅姟
 icegridadmin --Ice.Config=G:\00workspace\00workspace\01JCXT\IceServer\src\main\resources\ice\registry.cfg -e "application update  'G:\00workspace\00workspace\01JCXT\IceServer\src\main\resources\ice\grid.xml' "
