65d5c5b13001d63bcb91df52c168ec3d160140e8
Amy <luomouren@foxmail.com>
2017-02-07 17:17:23 +0800

    SwitchCallbackPrxCache添加sn

--- src/main/java/com/weisi/Server/bean/SwitchCallbackPrxCache.java
+++ src/main/java/com/weisi/Server/bean/SwitchCallbackPrxCache.java
@@ -6,11 +6,12 @@ import com.weisi.Server.switcher.ISwitchCallbackPrx;
  * ice客户端缓存信息
  */
 public class SwitchCallbackPrxCache {
-
+	private static final long serialVersionUID = -6579533328390250520L;
 	private ISwitchCallbackPrx iSwitchCallbackPrx;
-	private String ip;
-	private long lastTime;
-	private int port;
+	private String ip;//客户端连接ip
+	private String sn;//客户端唯一标识
+	private long lastTime;//客户端最后一次请求时间
+	private int port;//客户端连接端口
 
 	public ISwitchCallbackPrx getiSwitchCallbackPrx() {
 		return iSwitchCallbackPrx;
@@ -42,6 +43,14 @@ public class SwitchCallbackPrxCache {
 
 	public void setPort(int port) {
 		this.port = port;
+	}
+
+	public String getSn() {
+		return sn;
+	}
+
+	public void setSn(String sn) {
+		this.sn = sn;
 	}
 
 }
